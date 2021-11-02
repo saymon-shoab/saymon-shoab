71d70e94c7c1863adff23729f2825ed80af6900c
saymon shoab <mdsaymonshoab@gmail.com>
2021-11-02 18:24:09 +0600

    update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -12,10 +12,10 @@ const About = () => {
             <div className="w-full lg:w-4/6 text-white">
                 <h1 className="text-center  divide-y divide-yellow-500 text-4xl ">About Me</h1>
                 
-                <p className="text-white mt-5 ml-4">I'm an enthusiastic, responsible and hard working person. I’m always hungry for new technologies, a great fascination with programming and web development. I’m comfortable with Javascript (ES6), Node.js, React.js, Next.js, Express.Js, Bootstrap, Tailwind.css, Material UI, HTML5, CSS3, Firebase, and MongoDB.</p>
-                <p className="mt-5 ml-4">I want to be Professional Full Stack Developer, it's my dream.I love to learn new tech and new languages.</p>
-                
-                <h1 className="text-center text-4xl ">Basic Info</h1>
+                <p className="text-white mt-5 ml-4 text-2xl">I'm an enthusiastic, responsible and hard working person. I’m always hungry for new technologies, a great fascination with programming and web development. I’m comfortable with Javascript (ES6), Node.js, React.js, Next.js, Express.Js, Bootstrap, Tailwind.css, Material UI, HTML5, CSS3, Firebase, and MongoDB.</p>
+                <p className="mt-5 ml-4 text-2xl">I want to be Professional Full Stack Developer, it's my dream.I love to learn new tech and new languages.</p>
+{/*                 
+                <h1 className="text-center text-4xl ">Basic Info</h1> */}
             </div>
             </div>    
       
